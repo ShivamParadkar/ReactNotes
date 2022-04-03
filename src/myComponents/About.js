0b3de207e415ec1d44de0d6cdef8038d7e24f788
Shivam <Shivam83paraddkar@gmail.com>
2022-04-03 17:23:06 +0530

    adding components

---
+++ src/myComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        This is about component
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis, quisquam ipsum. Vero laudantium similique quis sequi reiciendis omnis. Excepturi autem soluta modi eligendi rerum qui officia a pariatur ad cupiditate.
+        </p>
+    </div>
+  )
+}
